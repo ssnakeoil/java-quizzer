817a7bbb0dceff37d951469cf5543cafab89c91e
Stefi <114958335+ssnakeoil@users.noreply.github.com>
2022-11-28 00:05:40 -0500

    added variables

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,6 @@
 // Establishing some variables
+var quizContainer = document.getElementById("quizContainer");
+var quiz = document.getElementById("quiz");
+var questions = document.getElementById("questions");
+var choiceButtons = document.getElementById("choiceButtons");
+var timer = document.getElementById("timer");
